6b7ad266a85c7dd60616c735b02ac676c771b7ef
kadas36 <oduorthomas14@gmail.com>
2020-11-14 17:44:36 +0300

    Add initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
